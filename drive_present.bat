2a2e572c8c21901faa9af2b319babaca68fb9dd9
Gustavo Espindola <yhoazk@gmail.com>
2017-07-26 14:06:37 -0500

    Update drive_present.bat

--- drive_present.bat
+++ drive_present.bat
@@ -1,13 +1,17 @@
-:: This script verifyes that the drive T is currently mounted and prints Drive T found if it's the case
-
 @echo off
 :loop
-  ::wmic path CIM_LogicalDevice where "Description like 'USB%%'" get /value > usb_log.txt
-  wmic logicaldisk get name | find "T:"  :: regresa una lista de todos los drives conectados y busca por el drive T
+rem :: This script verifyes that the drive T is currently mounted and prints Drive T found if it's the case
+rem  ::wmic path CIM_LogicalDevice where "Description like 'USB%%'" get /value > usb_log.txt
+  wmic logicaldisk get name | find "G:"
+  :: regresa una lista de todos los drives conectados y busca por el drive T
 
 :: get a list of all the properties of the disks
 :: wmic logicaldisk get
 
-  if  %errorlevel% == 0 echo "Drive t found"
+  if  %errorlevel% == 0 (
+      echo "Drive g found"
+  ) else (
+      echo "Drive NOT g found"
+  )
   timeout /t 2
-goto loop
+goto loo
